b34a686f13d1545effa6e036f3df88bbcdac9a48
CaueAnjos <141049846+CaueAnjos@users.noreply.github.com>
2025-08-01 15:32:51 -0300

    feat(devtools): add hidden files by default for fzf

--- devtools.nix
+++ devtools.nix
@@ -42,7 +42,7 @@
   programs.fzf = {
     enable = true;
     changeDirWidgetCommand = "fd --type dir --hidden";
-    defaultCommand = "fd --type f";
+    defaultCommand = "fd --type f --hidden";
     defaultOptions = [
       "--height 40%"
       "--border"
