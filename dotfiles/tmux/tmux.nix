d93441e74f0fd07e46ccb77f8e39210ee327c09a
CaueAnjos <141049846+CaueAnjos@users.noreply.github.com>
2025-08-29 19:51:29 -0300

    feat(tmux): configure tmux

--- dotfiles/tmux/tmux.nix
+++ dotfiles/tmux/tmux.nix
@@ -1,5 +1,17 @@
-{
+{pkgs, ...}: {
   programs.tmux = {
     enable = true;
+    shell = "${pkgs.zsh}/bin/zsh";
+    newSession = true;
+    keyMode = "vi";
+    disableConfirmationPrompt = true;
+    customPaneNavigationAndResize = true;
+    extraConfig = ''
+      bind r source-file ~/.config/tmux/tmux.conf
+    '';
+
+    plugins = with pkgs.tmuxPlugins; [
+      tokyo-night-tmux
+    ];
   };
 }
