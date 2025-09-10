0fbd973450c68380e86de195f4a4cddba3cccb3d
CaueAnjos <141049846+CaueAnjos@users.noreply.github.com>
2025-09-10 18:39:51 -0300

    style(git): better style for configuration

--- home-manager/git/default.nix
+++ home-manager/git/default.nix
@@ -1,14 +1,16 @@
-{...}: {
-  programs.git = {
-    enable = true;
-    userName = "CaueAnjos";
-    userEmail = "141049846+CaueAnjos@users.noreply.github.com";
-    aliases = {
-      st = "status -s";
-      lg = "log --oneline --graph --decorate";
+{
+  programs = {
+    git = {
+      enable = true;
+      userName = "CaueAnjos";
+      userEmail = "141049846+CaueAnjos@users.noreply.github.com";
+      aliases = {
+        st = "status -s";
+        lg = "log --oneline --graph --decorate";
+      };
     };
-  };
 
-  programs.gh.enable = true;
-  programs.lazygit.enable = true;
+    gh.enable = true;
+    lazygit.enable = true;
+  };
 }
