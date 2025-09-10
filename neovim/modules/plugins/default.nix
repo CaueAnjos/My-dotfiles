6a91fabb796f0945fa18e5d9c6a498538439289d
CaueAnjos <141049846+CaueAnjos@users.noreply.github.com>
2025-09-10 19:25:26 -0300

    feat(neovim): add automaticaly all files in the plugins directory and subdirecotrys

--- neovim/modules/plugins/default.nix
+++ neovim/modules/plugins/default.nix
@@ -1,6 +1,12 @@
-{
-  imports = [
-    ./smear-cursor.nix
-    ./tiny-inline-diagnostic.nix
-  ];
+let
+  importAllNixRecursive = dir: let
+    inherit (builtins) readDir filter match attrNames;
+    entries = readDir dir;
+    nixFiles = filter (name: match ".*\\.nix$" name != null && name != "default.nix") (attrNames entries);
+    directories = filter (name: entries.${name} == "directory") (attrNames entries);
+  in
+    (map (name: dir + "/${name}") nixFiles)
+    ++ (builtins.concatLists (map (d: importAllNixRecursive (dir + "/${d}")) directories));
+in {
+  imports = importAllNixRecursive ./.;
 }
