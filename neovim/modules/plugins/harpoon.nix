961fd5a4e2a90b0d907be16c164e52c184aad80a
CaueAnjos <141049846+CaueAnjos@users.noreply.github.com>
2025-09-10 22:20:23 -0300

    feat(neovim): add harpoon plugin

---
+++ neovim/modules/plugins/harpoon.nix
@@ -0,0 +1,11 @@
+{
+  vim.navigation.harpoon = {
+    enable = true;
+    mappings = {
+      file4 = "<leader>tj";
+      file3 = "<leader>tk";
+      file2 = "<leader>tl";
+      file1 = "<leader>tç";
+    };
+  };
+}
