bf84f27fdfb5993033a655435a0b70d45272b6ab
CaueAnjos <141049846+CaueAnjos@users.noreply.github.com>
2025-09-11 19:32:17 -0300

    feat(neovim): change harpoon keymaps

--- neovim/modules/plugins/harpoon.nix
+++ neovim/modules/plugins/harpoon.nix
@@ -2,10 +2,10 @@
   vim.navigation.harpoon = {
     enable = true;
     mappings = {
-      file4 = "<leader>tj";
-      file3 = "<leader>tk";
-      file2 = "<leader>tl";
-      file1 = "<leader>tç";
+      file1 = "<leader>tj";
+      file2 = "<leader>tk";
+      file3 = "<leader>tl";
+      file4 = "<leader>tç";
     };
   };
 }
