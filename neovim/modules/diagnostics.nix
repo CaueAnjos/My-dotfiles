dd491519a0e3688669d07cb190f964534a4ced47
CaueAnjos <141049846+CaueAnjos@users.noreply.github.com>
2025-09-18 21:33:06 -0300

    feat(neovim): add diagnostics info icon

--- neovim/modules/diagnostics.nix
+++ neovim/modules/diagnostics.nix
@@ -10,6 +10,7 @@
             [vim.diagnostic.severity.ERROR] = "󰅚 ",
             [vim.diagnostic.severity.WARN] = "󰀪 ",
             [vim.diagnostic.severity.HINT] = " ",
+            [vim.diagnostic.severity.INFO] = "󰛩 ",
           }
         '';
         linehl = lib.mkLuaInline ''
