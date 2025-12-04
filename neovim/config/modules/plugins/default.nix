20f3384a1e3323b2178e3b61cdf9bfbf920c5d85
CaueAnjos <141049846+CaueAnjos@users.noreply.github.com>
2025-12-04 18:35:34 -0300

    fix: change plugins/default.nix to have more control

--- neovim/config/modules/plugins/default.nix
+++ neovim/config/modules/plugins/default.nix
@@ -1,12 +1,14 @@
-let
-  importAllNixRecursive = dir: let
-    inherit (builtins) readDir filter match attrNames;
-    entries = readDir dir;
-    nixFiles = filter (name: match ".*\\.nix$" name != null && name != "default.nix") (attrNames entries);
-    directories = filter (name: entries.${name} == "directory") (attrNames entries);
-  in
-    (map (name: dir + "/${name}") nixFiles)
-    ++ (builtins.concatLists (map (d: importAllNixRecursive (dir + "/${d}")) directories));
-in {
-  imports = importAllNixRecursive ./.;
+{
+  imports = [
+    ./default.nix
+    ./git.nix
+    ./luasnip.nix
+    ./marks.nix
+    ./mini.nix
+    ./smear-cursor.nix
+    ./telescope.nix
+    ./tiny-inline-diagnostic.nix
+    ./todo-comments.nix
+    ./yazi.nix
+  ];
 }
