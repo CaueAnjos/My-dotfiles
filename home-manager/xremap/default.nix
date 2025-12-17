9740ccd27906fdf8d39f73ab10d791e619a2ccbc
CaueAnjos <141049846+CaueAnjos@users.noreply.github.com>
2025-12-17 20:09:41 -0300

    fix(home/xremap): fix xremap

--- home-manager/xremap/default.nix
+++ home-manager/xremap/default.nix
@@ -1,11 +1,22 @@
-{
-  services.xremap = {
-    enable = true;
-    config.modmap = [
-      {
-        name = "Global";
-        remap = {"CapsLock" = "Ctrl";};
-      }
-    ];
-  };
+{inputs, ...}: {
+  imports = [
+    inputs.xremap-flake.homeManagerModules.default
+    {
+      services.xremap = {
+        enable = true;
+        withGnome = true;
+        config.modmap = [
+          {
+            name = "Global";
+            remap = {
+              "CapsLock" = {
+                held = "Ctrl_R";
+                alone = "Esc";
+              };
+            };
+          }
+        ];
+      };
+    }
+  ];
 }
