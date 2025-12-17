299328f9bc4612d1a197f76013a0ddbb00377dda
CaueAnjos <141049846+CaueAnjos@users.noreply.github.com>
2025-12-17 17:54:43 -0300

    fix(nixos/users): fix uinput group for xremap

--- nixos/users.nix
+++ nixos/users.nix
@@ -1,6 +1,16 @@
 {
-  users.users.kawid = {
-    isNormalUser = true;
-    extraGroups = ["wheel"]; # Enable ‘sudo’ for the user.
+  boot.kernelModules = ["uinput"];
+
+  services.udev.extraRules = ''
+    KERNEL=="uinput", MODE="0660", GROUP="uinput"
+    KERNEL=="event*", SUBSYSTEM=="input", MODE="0660", GROUP="input"
+  '';
+  users = {
+    groups.uinput = {};
+    groups.input = {};
+    users.kawid = {
+      isNormalUser = true;
+      extraGroups = ["wheel" "input" "uinput"]; # Enable ‘sudo’ for the user.
+    };
   };
 }
