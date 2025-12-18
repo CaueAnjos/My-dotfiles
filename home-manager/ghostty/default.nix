88dc08ab23eb576b69e49fb23ee43e1be3e16d5d
CaueAnjos <141049846+CaueAnjos@users.noreply.github.com>
2025-12-18 19:35:58 -0300

    feat(home/ghotty): add opacity to ghostty

--- home-manager/ghostty/default.nix
+++ home-manager/ghostty/default.nix
@@ -5,5 +5,15 @@
 
   fonts.fontconfig.enable = true;
 
-  programs.ghostty.enable = true;
+  programs.ghostty = {
+    enable = true;
+    enableZshIntegration = false;
+    settings = {
+      font-size = 15;
+      cursor-style = "block";
+      cursor-opacity = 0.5;
+      mouse-hide-while-typing = true;
+      background-opacity = 0.5;
+    };
+  };
 }
