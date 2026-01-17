5ba5288a3f7d214e32e4adc06f781dbc4474fa4c
CaueAnjos <141049846+CaueAnjos@users.noreply.github.com>
2026-01-17 17:05:38 -0300

    fix(home): remove autoaccept for syncthing and add predefined paths

--- home-manager/syncthing/default.nix
+++ home-manager/syncthing/default.nix
@@ -7,9 +7,23 @@
         password = "0914";
       };
       devices = {
-        "CelularCaue" = {
+        "phone" = {
           id = "R4KBHZE-4PSK5PL-3BZDH2P-25NVAFT-J5LG36W-YJRU6TW-GH3UWPQ-DG7ZMAI";
-          autoAcceptFolders = true;
+          autoAcceptFolders = false;
+        };
+      };
+      folders = {
+        obsidianVaults = {
+          enable = true;
+          lable = "Obsidain Vaults";
+          path = "/home/kawid/Documents/obsidianVaults";
+          devices = ["phone"];
+        };
+        images = {
+          enable = true;
+          lable = "Images";
+          path = "/home/kawid/Documents/Images";
+          devices = ["phone"];
         };
       };
     };
