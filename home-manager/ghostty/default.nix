9b231a2ca4e502698d21e87ec2d183a5e6277303
CaueAnjos <141049846+CaueAnjos@users.noreply.github.com>
2026-01-17 10:27:40 -0300

    fix(home): change ghostty font
    This fixes missing characters

--- home-manager/ghostty/default.nix
+++ home-manager/ghostty/default.nix
@@ -1,6 +1,7 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [
     nerd-fonts.jetbrains-mono
+    noto-fonts-lgc-plus
   ];
 
   fonts.fontconfig.enable = true;
@@ -8,6 +9,10 @@
   programs.ghostty = {
     enable = true;
     settings = {
+      font-family = [
+        "JetBrainsMono Nerd Font"
+        "Noto Sans Symbols 2"
+      ];
       font-size = 13;
       cursor-style = "block";
       mouse-hide-while-typing = true;
