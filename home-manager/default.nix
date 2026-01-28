ff226bdd500592460464d0b74d428d2d662c3d16
CaueAnjos <141049846+CaueAnjos@users.noreply.github.com>
2026-01-28 17:41:15 -0300

    fix(home): fix home-manager installation

--- home-manager/default.nix
+++ home-manager/default.nix
@@ -1,5 +1,11 @@
-{inputs, ...}: {
-  flake.homeConfigurations."kawid" = inputs.home-manager.lib.homeManagerConfiguration {
+{inputs, ...}: let
+  inherit (inputs) home-manager;
+in {
+  imports = [
+    home-manager.flakeModules.home-manager
+  ];
+
+  flake.homeConfigurations."kawid" = home-manager.lib.homeManagerConfiguration {
     pkgs = inputs.nixpkgs.legacyPackages.x86_64-linux;
     modules = [
       ./home.nix
