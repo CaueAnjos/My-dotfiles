82db88c2fddf8a4342be012bfcaa461f6a3c3e74
CaueAnjos <141049846+CaueAnjos@users.noreply.github.com>
2026-01-28 17:06:32 -0300

    refactor(home): move xremap outside import

--- home-manager/xremap/default.nix
+++ home-manager/xremap/default.nix
@@ -27,45 +27,44 @@
 in {
   imports = [
     inputs.xremap-flake.homeManagerModules.default
-    {
-      services.xremap = {
-        enable = true;
-        withGnome = true;
-        config.modmap = [
-          {
-            name = "Global";
-            remap = {
-              "CapsLock" = {
-                held = "Ctrl_R";
-                alone = "Esc";
-              };
-            };
-          }
-        ];
-
-        config.keymap = [
-          {
-            name = "Launch";
-            remap = {
-              "Super-T".launch = [(lib.getExe pkgs.ghostty) "+new-window"];
-              "Super-B".launch = [(lib.getExe pkgs.firefox)];
-              "Super-Space".launch = [(lib.getExe pkgs.walker)];
-              "Alt-l".launch = [(lib.getExe pkgs.hyprlock)];
-              "Alt-P".launch = [(lib.getExe pike-wallpaper)];
-            };
-          }
-          {
-            name = "Global";
-            remap = {
-              "Ctrl-M" = "Enter";
-              Ctrl-Shift-h = "Left";
-              Ctrl-Shift-j = "Down";
-              Ctrl-Shift-k = "Up";
-              Ctrl-Shift-l = "Right";
-            };
-          }
-        ];
-      };
-    }
   ];
+
+  services.xremap = {
+    enable = true;
+    withGnome = true;
+    config.modmap = [
+      {
+        name = "Global";
+        remap = {
+          "CapsLock" = {
+            held = "Ctrl_R";
+            alone = "Esc";
+          };
+        };
+      }
+    ];
+
+    config.keymap = [
+      {
+        name = "Launch";
+        remap = {
+          "Super-T".launch = [(lib.getExe pkgs.ghostty) "+new-window"];
+          "Super-B".launch = [(lib.getExe pkgs.firefox)];
+          "Super-Space".launch = [(lib.getExe pkgs.walker)];
+          "Alt-l".launch = [(lib.getExe pkgs.hyprlock)];
+          "Alt-P".launch = [(lib.getExe pike-wallpaper)];
+        };
+      }
+      {
+        name = "Global";
+        remap = {
+          "Ctrl-M" = "Enter";
+          Ctrl-Shift-h = "Left";
+          Ctrl-Shift-j = "Down";
+          Ctrl-Shift-k = "Up";
+          Ctrl-Shift-l = "Right";
+        };
+      }
+    ];
+  };
 }
