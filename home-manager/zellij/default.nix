7cf4c4dc48bb9c01a0463d2416471d0a79adfb4e
CaueAnjos <141049846+CaueAnjos@users.noreply.github.com>
2026-02-09 21:08:32 -0300

    feat(home): change zellij appearence

--- home-manager/zellij/default.nix
+++ home-manager/zellij/default.nix
@@ -14,10 +14,47 @@
 in {
   programs.zellij = {
     enable = true;
-    extraConfig = ''
-      keybinds {
-      }
-    '';
+    layouts.dev.layout = {
+      default_tab_template._children = [
+        {
+          children = {};
+          pane = {
+            size = 1;
+            borderless = true;
+            plugin = {
+              location = "https://github.com/dj95/zjstatus/releases/latest/download/zjstatus.wasm";
+
+              format_left = "{mode} #[fg=#${palette.base0D},bold]{session}";
+              format_center = "{tabs}";
+              format_right = "{command_git_branch} {mode}";
+              format_space = "";
+
+              border_enabled = "false";
+              border_char = "─";
+              border_format = "#[fg=#6C7086]{char}";
+              border_position = "top";
+
+              hide_frame_for_single_pane = "true";
+
+              mode_normal = "#[bg=blue]";
+              mode_tmux = "#[bg=#ffc387]";
+
+              tab_normal = "#[fg=#6C7086] {name}";
+              tab_active = "#[fg=#9399B2,bold,italic] {name}";
+
+              command_git_branch_command = "git rev-parse --abbrev-ref HEAD";
+              command_git_branch_format = "#[fg=blue] {stdout}";
+              command_git_branch_interval = "10";
+              command_git_branch_rendermode = "static";
+
+              datetime = "#[fg=#6C7086,bold] {format} ";
+              datetime_format = "%A, %d %b %Y %H:%M";
+              datetime_timezone = "Europe/Berlin";
+            };
+          };
+        }
+      ];
+    };
     settings = {
       keybinds = {
         unbind._args = ["Ctrl g" "Ctrl p" "Ctrl n" "Ctrl o" "Ctrl t"];
