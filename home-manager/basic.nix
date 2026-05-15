b0b84e72a6d668a9326cd9d31969c3571bad9f5c
CaueAnjos <141049846+CaueAnjos@users.noreply.github.com>
2026-05-15 00:13:26 -0300

    feat: add basic apps

--- home-manager/basic.nix
+++ home-manager/basic.nix
@@ -1,5 +1,11 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [
     nautilus
+    gnome-clocks
+    vlc
   ];
+
+  programs.discord.enable = true;
+  programs.thunderbird.enable = true;
+  services.easyeffects.enable = true;
 }
