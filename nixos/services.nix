b4573b0c10ac9fe86fb1758630e6f3d793d9ea9d
CaueAnjos <141049846+CaueAnjos@users.noreply.github.com>
2026-06-04 12:15:02 -0300

    feat: add blocky proxy service

--- nixos/services.nix
+++ nixos/services.nix
@@ -23,5 +23,37 @@
     udisks2.enable = true;
 
     openssh.enable = true;
+    blocky = {
+      enable = true;
+      settings = {
+        ports.dns = 53;
+        upstreams.groups.default = [
+          "https://one.one.one.one/dns-query"
+        ];
+
+        bootstrapDns = {
+          upstream = "https://one.one.one.one/dns-query";
+          ips = ["1.1.1.1" "1.0.0.1"];
+        };
+
+        blocking = {
+          denylists = {
+            ads = ["https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts"];
+            adult = ["https://blocklistproject.github.io/Lists/porn.txt"];
+          };
+          clientGroupsBlock = {
+            default = ["ads" "adult"];
+          };
+        };
+
+        caching = {
+          minTime = "5m";
+          maxTime = "30m";
+          prefetching = true;
+        };
+      };
+    };
   };
+
+  networking.nameservers = ["127.0.0.1"];
 }
