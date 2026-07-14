66016ad715d7fc7c5fae73fb1e5ea761dbd88eb0
CaueAnjos <141049846+CaueAnjos@users.noreply.github.com>
2026-07-14 19:03:15 -0300

    feat: add jujutsu

--- home-manager/git/default.nix
+++ home-manager/git/default.nix
@@ -1,18 +1,26 @@
-{
+let
+  user = {
+    name = "CaueAnjos";
+    email = "141049846+CaueAnjos@users.noreply.github.com";
+  };
+in {
   programs = {
     git = {
       enable = true;
-      settings = {
-        user = {
-          name = "CaueAnjos";
-          email = "141049846+CaueAnjos@users.noreply.github.com";
-        };
-      };
+      settings = {inherit user;};
       includes = [
         {
           contents.init.defaultBranch = "main";
         }
       ];
+    };
+
+    jujutsu = {
+      enable = true;
+      settings = {
+        inherit user;
+        ui.default-command = "log";
+      };
     };
 
     gh.enable = true;
